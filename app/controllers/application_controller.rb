4d4bea66da23c1f347536e2598f3ede91cf801d0
細川貴英 <TAKAHIDE@H.local>
2014-09-11 15:18:16 +0900

    remove un~ files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,17 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    "/posts"
+  end
+
+  def after_sign_out_path_for(resource)
+    "/posts"
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:subdomain, :username, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:subdomain, :username, :email, :password, :remember_me) }
   end
 end
+
